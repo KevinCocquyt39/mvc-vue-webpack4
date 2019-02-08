082d5c5f94509d3cff7dfd14de2c294bb13b6100
Kevin Cocquyt <kevin_cocquyt@outlook.com>
2019-02-08 10:34:17 +0100

    Added new component loader file

---
+++ js/components/home/index.js
@@ -0,0 +1,14 @@
+﻿import Vue from "vue";
+import FirstComponent from "../FirstComponent.vue";
+
+var vm = new Vue({
+    el: "#nt-page-home",
+    components: {
+        FirstComponent
+    }
+});
+
+vm.$refs.myFirstComponent.list = [
+    { id: 1, name: "Acme" },
+    { id: 2, name: "Corp" }
+];
